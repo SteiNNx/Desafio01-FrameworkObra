09b16bde2f8ac2f2675c45103560e7da435d043f
Jorge Reyes <jorge_irving_15@hotmail.com>
2022-03-16 20:29:13 -0300

    Dummy

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,116 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a-layout class="layout">
+      <a-layout-header style="height: 10rem; display: flex; justify-content: center">
+        <Navbar />
+      </a-layout-header>
+      <a-layout-content style="padding: 0 50px">
+        <a-row type="flex">
+          <a-col :span="24">
+            <a-table :columns="columns" :data-source="data">
+              <a slot="name" slot-scope="text">{{ text }}</a>
+              <span slot="customTitle"><a-icon type="smile-o" /> Name</span>
+              <span slot="tags" slot-scope="tags">
+                <a-tag
+                  v-for="tag in tags"
+                  :key="tag"
+                  :color="
+                    tag === 'loser' ? 'volcano' : tag.length > 5 ? 'geekblue' : 'green'
+                  "
+                >
+                  {{ tag.toUpperCase() }}
+                </a-tag>
+              </span>
+              <span slot="action" slot-scope="text, record">
+                <a>Invite 一 {{ record.name }}</a>
+                <a-divider type="vertical" />
+                <a>Delete</a>
+                <a-divider type="vertical" />
+                <a class="ant-dropdown-link"> More actions <a-icon type="down" /> </a>
+              </span>
+            </a-table>
+          </a-col>
+
+          <a-col :span="24">
+            <a-button type="primary" @click="handleOnClickModal(true)"
+              >Abrir Modal</a-button
+            >
+            <a-modal
+              v-model="isOpenModal"
+              title="Ejemplo Modal"
+              @ok="handleOnClickModal(false)"
+            >
+            </a-modal>
+          </a-col>
+        </a-row>
+      </a-layout-content>
+      <a-layout-footer>Footer</a-layout-footer>
+    </a-layout>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from "@/components/Navbar";
+const columns = [
+  {
+    dataIndex: "name",
+    key: "name",
+  },
+  {
+    dataIndex: "age",
+    key: "age",
+  },
+  {
+    dataIndex: "address",
+    key: "address",
+  },
+  {
+    key: "tags",
+    dataIndex: "tags",
+  },
+];
 
+const data = [
+  {
+    key: "1",
+    name: "John Brown",
+    age: 32,
+    address: "New York No. 1 Lake Park",
+    tags: ["nice", "developer"],
+  },
+  {
+    key: "2",
+    name: "Jim Green",
+    age: 42,
+    address: "London No. 1 Lake Park",
+    tags: ["loser"],
+  },
+  {
+    key: "3",
+    name: "Joe Black",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
+    tags: ["cool", "teacher"],
+  },
+];
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Navbar,
+  },
+  data() {
+    return {
+      isOpenModal: false,
+      data,
+      columns,
+    };
+  },
+  methods: {
+    handleOnClickModal(newStateIsOpenModal) {
+      this.isOpenModal = newStateIsOpenModal;
+    },
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <a-menu mode="horizontal" @click="handleClick">
+    <a-menu-item key="1">
+      <a-icon type="mail" />
+      Navigation One
+    </a-menu-item>
+    <a-menu-item key="2">
+      <a-icon type="calendar" />
+      Navigation Two
+    </a-menu-item>
+    <a-sub-menu key="sub1">
+      <span slot="title"><a-icon type="appstore" /><span>Navigation Three</span></span>
+      <a-menu-item key="3"> Option 3 </a-menu-item>
+      <a-menu-item key="4"> Option 4 </a-menu-item>
+      <a-sub-menu key="sub1-2" title="Submenu">
+        <a-menu-item key="5"> Option 5 </a-menu-item>
+        <a-menu-item key="6"> Option 6 </a-menu-item>
+      </a-sub-menu>
+    </a-sub-menu>
+  </a-menu>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  methods: {
+    handleClick(e) {
+      console.log("click ", e);
+    },
+  },
+};
+</script>
+
+<style></style>
